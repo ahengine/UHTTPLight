dfca75bf7ec0f83e8e82654136368afb0b20150f
ahengine <ahengine@live.com>
2024-10-19 18:42:26 +0330

    Update UHTTPConfig.cs

--- Runtime/UHTTPConfig.cs
+++ Runtime/UHTTPConfig.cs
@@ -7,11 +7,11 @@ namespace UHTTPLight
     public class UHTTPConfig : ScriptableObject
     {      
         [Tooltip("UHTTPConfig (Don't change asset name), This asset must be in resources folder.")]
-        public string baseURL { get; set; }
-        public List<KeyValuePair<string,string>> defaultHeaders { get; set; } = new List<KeyValuePair<string, string>>() {
+        [field:SerializeField] public string baseURL { get; set; }
+        [field:SerializeField] public List<KeyValuePair<string,string>> defaultHeaders { get; set; } = new List<KeyValuePair<string, string>>() {
             new KeyValuePair<string, string>("Content-Type", "application/json"),
             new KeyValuePair<string, string>("Accept", "application/json"),
         };
-        public bool useBearerPrefixAuthHeader { get; set; } = true;
+        [field:SerializeField] public bool useBearerPrefixAuthHeader { get; set; } = true;
     }
 }
